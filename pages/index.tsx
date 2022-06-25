aa373be5e2663505777ae0977d75397b6c8fff2b
nikmace <nikmacentosh@gmail.com>
2022-06-25 13:56:30 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our Smart Contract Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
