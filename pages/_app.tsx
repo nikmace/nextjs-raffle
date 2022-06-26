f1370b241ed81a02d524dff004bd6c959f5dc2b2
nikmace <nikmacentosh@gmail.com>
2022-06-26 09:35:07 +0300

    Added providers for app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <NotificationProvider>
+        <Component {...pageProps} />
+      </NotificationProvider>
+    </MoralisProvider>
+  );
 }
 
 export default MyApp;
